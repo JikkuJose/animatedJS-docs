dc020f10a0554b4753774c0dc2896acd3a1836e0
Jikku Jose <jikkujose@gmail.com>
2017-11-17 13:35:04 +0530

    Add header

---
+++ src/App/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import NestedContainer from "core/NestedContainer"
+
+function Header() {
+  return (
+    <NestedContainer
+      className="ch bb b--black-10"
+      innerClassName="f1 open-sans h4 cv blue"
+    >
+      AnimatedJS
+    </NestedContainer>
+  )
+}
+
+export default Header
