96f63befea0501fe530047fff4d099693f9ec06b
Jikku Jose <jikkujose@gmail.com>
2017-11-17 13:50:00 +0530

    Update App

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,17 @@
 import React from "react"
 
+import SiteWrapper from "core/SiteWrapper"
+import Header from "./Header"
+import One from "./examples/One"
+
 class App extends React.Component {
   render() {
-    return <div className="bg-red w3 h3" />
+    return (
+      <SiteWrapper>
+        <Header />
+        <One />
+      </SiteWrapper>
+    )
   }
 }
 
